cc5d358da146b7863293abcc8b1e067bd2674882
mgoszcz <marcin.goszczynski88@gmail.com>
2020-03-25 15:40:08 +0100

    Unittest finished

--- todo_list/client_unittest/test.py
+++ todo_list/client_unittest/test.py
@@ -6,7 +6,7 @@ INITIAL_TASKS = [
     {'description': 'Need to find a good Python tutorial on the web', 'done': False, 'id': 2, 'title': 'Learn Python'}]
 
 NEW_TASK = {'description': 'Opis nowego zadania', 'done': False, 'id': 3, 'title': 'nowe zadanie'}
-MODIFIED_TASK = {'description': 'Zmodyfikowany opis nowego zadania', 'done': True,
+MODIFIED_TASK = {'description': 'Zmodyfikowany opis nowego zadania', 'done': True, 'id': 3,
                  'title': 'zmodyfikowane nowe zadanie'}
 
 
@@ -22,20 +22,16 @@ class TestTodoApi(unittest.TestCase):
         self.assertEqual(client.get_task(1), INITIAL_TASKS[0], 'Verify first initial task')
         self.assertEqual(client.get_task(2), INITIAL_TASKS[1], 'Verify second initial task')
 
-    def test_add_task(self):
+    def test_tasks_manipulation(self):
         client.add_task(title=NEW_TASK['title'], description=NEW_TASK['description'])
         tasks = client.get_tasks()
         self.assertEqual(len(tasks), len(INITIAL_TASKS + [NEW_TASK]), 'Verify count of tasks after adding new task')
         self.assertEqual(tasks, INITIAL_TASKS + [NEW_TASK], 'Verify tasks after adding new task')
-
-    def test_modify_task(self):
         client.modify_task(task_id=3, title=MODIFIED_TASK['title'], description=MODIFIED_TASK['description'],
                            done=MODIFIED_TASK['done'])
         tasks = client.get_tasks()
         self.assertEqual(len(tasks), len(INITIAL_TASKS + [MODIFIED_TASK]), 'Verify count of tasks after modifying task')
         self.assertEqual(tasks, INITIAL_TASKS + [MODIFIED_TASK], 'Verify tasks after modifying task')
-
-    def test_delete_task(self):
         client.delete_task(3)
         tasks = client.get_tasks()
         self.assertEqual(len(tasks), len(INITIAL_TASKS), 'Verify count of tasks after removing task')
